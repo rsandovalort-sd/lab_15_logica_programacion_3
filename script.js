66e6f1d6f64c4ded1374c3027f1b3d3aa811409a
Ricardo <rsandovalort@gmail.com>
2025-12-10 17:46:54 -0500

    segundo ajuste

--- script.js
+++ script.js
@@ -15,20 +15,14 @@ function calcularFactorial(){
     card.style.width = "18rem";
     card.style.boxShadow = "4px 4px 4px 4px #424040ff";
 
-    const img = document.createElement("img");
-    img.src = "image/factorial.png";
-    img.className = "card-img-top";
-    img.style = "width = 30px, height= 20px"
-    img.alt = "Simbolofactorial";
 
     const contenido_card = document.createElement("div");
     contenido_card.className = "card-body";
     const titulo_h2 = document.createElement("h2");
     titulo_h2.className = "card-title";
-    titulo_h2.textContent = factorial;
+    titulo_h2.textContent = numero + "! = " + factorial;
 
     contenido_card.appendChild(titulo_h2);
-    card.appendChild(img);
     card.appendChild(contenido_card);
     contenedor.appendChild(card);
 
